fcfa5cc744c4b3b15e46e6942bb963367fbd6827
Ety-Biswangri <etybgc02@gmail.com>
2022-02-20 02:39:53 +0600

    deposit, withdraw and balance has updated

--- js/functional.js
+++ js/functional.js
@@ -1,16 +1,42 @@
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
-    const inputValueInText = inputField.value;
-    const inputValue = parseFloat(inputValueInText);
+    const inputFieldValueInText = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueInText);
     inputField.value = '';
-    return inputValue;
+    return inputFieldValue;
 }
 
 function updateTotal(fieldId, amount) {
+    const totalField = document.getElementById(fieldId);
+    const previousTotalInText = totalField.innerText;
+    const previousTotal = parseFloat(previousTotalInText);
+    const newTotal = previousTotal + amount;
+    totalField.innerText = newTotal;
+}
 
+function updateBalance(amount, isAdding) {
+    const balanceField = document.getElementById('balance-total');
+    const balanceInText = balanceField.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if (isAdding == true) {
+        newBalance = previousBalance + amount;
+    }
+    else {
+        newBalance = previousBalance - amount;
+    }
+    balanceField.innerText = newBalance;
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
-    console.log(amount);
+    updateTotal('deposit-total', amount);
+    updateBalance(amount, true);
+})
+
+// handle withdraw
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total', amount);
+    updateBalance(amount, false);
 })
